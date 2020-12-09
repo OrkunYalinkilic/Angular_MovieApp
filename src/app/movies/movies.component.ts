f0bbf4cdab2955aff8e0a283e8b06b095440a631
Orkun Yalınkılıç <orkunyalinkilic69@gmail.com>
2020-12-09 13:27:08 +0300

    1-Yeni_Component_Ekleme

---
+++ src/app/movies/movies.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'movies',
+  templateUrl: 'movies.component.html',
+})
+export class MoviesComponent {
+
+}
+
+ // Burada oluşturdugumuz component'dan app.module un haberi olması gerekiyo.Declaration kısmına ekliyoruz.""
