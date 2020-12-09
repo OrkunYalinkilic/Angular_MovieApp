3aa9ff4ad54228ccb89e3f05027c69bb4f148630
Orkun Yalınkılıç <orkunyalinkilic69@gmail.com>
2020-12-09 17:49:44 +0300

    Lesson3:Component_Styles

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -3,6 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'movies',
   templateUrl: 'movies.component.html',
+  styles: [`
+         h2{
+         color:red;
+         }
+         `]
 })
 export class MoviesComponent {
 
